be4e0fad2737c7493d2f68e0b75f63dfbb405d8c
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2024-05-08 14:53:51 -0400

    oh well

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises'
 
-const DB_PATH = new URL('../db.json', import.meta.url).pathname
+const DB_PATH = new URL('../db.json', import.meta.url).pathname;
+
 
 export const getDB = async () => {
   const db = await fs.readFile(DB_PATH, 'utf-8')
